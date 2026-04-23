97331d4ed2113eba93cf994cda516e455c4bb1b4
Huy Nguyen <nguyenquanghuy07052004@gmail.com>
2026-04-23 16:30:36 +0700

    feat(config): Create AppConfig.java

---
+++ backend/src/main/java/org/example/backend/common/Constants.java
@@ -0,0 +1,9 @@
+package org.example.backend.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+    public static final String DATE_FORMAT = "dd.MM.yyyy";
+    public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss";
+}
