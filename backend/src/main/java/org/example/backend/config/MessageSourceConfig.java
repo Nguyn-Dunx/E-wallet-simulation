de59831890c22544e2393d09e3646d4ac7e1f595
Huy Nguyen <nguyenquanghuy07052004@gmail.com>
2026-04-24 23:38:50 +0700

    chore(config): separate message configuratioin

--- backend/src/main/java/org/example/backend/config/MessageSourceConfig.java
+++ backend/src/main/java/org/example/backend/config/MessageSourceConfig.java
@@ -3,43 +3,38 @@ package org.example.backend.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
+
 @Configuration
 public class MessageSourceConfig {
-
-    private final MessageSource messageSource;
-
-    public MessageSourceConfig(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
 
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setDefaultLocale(Locale.ENGLISH);
         messageSource.setFallbackToSystemLocale(false);
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setCacheSeconds(3600);
         return messageSource;
     }
 
     @Bean
-    public LocalValidatorFactoryBean getValidator() {
+    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-        bean.setValidationMessageSource(messageSource());
+        bean.setValidationMessageSource(messageSource);
         return bean;
     }
 
-    public String getMessage(String key) {
-        return messageSource().getMessage(key, new Object[]{}, LocaleContextHolder.getLocale());
-    }
-
-    public String getMessage(String key, Object... args) {
-        return messageSource().getMessage(key, args, LocaleContextHolder.getLocale());
+    @Bean
+    public AcceptHeaderLocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
     }
 }
