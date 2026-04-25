7c3a582de5864cc06ee468eb91e3cc9a1b17e211
HuyQuang075 <88672686+NoBugNoPain404@users.noreply.github.com>
2026-04-25 21:10:02 +0700

    Modify accounts table structure
    Updated the 'id' column to use a default UUID generator and removed 'pin_hash' and 'pin_failed_count' columns.

--- database/tables/auth_tables.sql
+++ database/tables/auth_tables.sql
@@ -1,11 +1,8 @@
 create table accounts(
-	id uuid not null primary key,
-	user_id uuid not null,
+	id uuid not null primary key default gen_random_uuid(),
 	password_hash text not null,
-	pin_hash text,
 	status varchar(20) not null default 'ACTIVE',
 	login_failed_count int not null default 0,
-	pin_failed_count int not null default 0,
 	password_change_at timestamptz,
 	create_at timestamptz default now(),
 	updated_at timestamptz
