66ebe56ec7895fa72fe7bebf780435041e8365d3
HuyQuang075 <88672686+NoBugNoPain404@users.noreply.github.com>
2026-04-26 09:29:38 +0700

    Reformat users_table.sql for consistency and clarity

--- database/tables/users_table.sql
+++ database/tables/users_table.sql
@@ -1,11 +1,27 @@
-create table users(
-	id uuid not null primary key references accounts(id) on delete cascade,
-	full_name varchar(100) not null,
-	phone varchar(20) not null unique,
-	ekyc_status varchar(20) not null default 'UNVERIFIED',
-	avatar_url text,
-	status varchar(20) not null default 'ACTIVE',
-	created_at timestamptz not null default now(),
-	updated_at timestamptz,
-	deleted_at timestamptz
-)
+-- USERS
+create table users (
+    id uuid primary key references accounts(id) on delete cascade,
+
+    full_name varchar(100) not null,
+    phone varchar(20) not null unique,
+
+    ekyc_status varchar(20) not null default 'UNVERIFIED',
+    avatar_url text,
+
+    created_at timestamptz not null default now(),
+    updated_at timestamptz,
+    deleted_at timestamptz
+);
+
+-- ADMINS
+create table admin (
+    id uuid primary key references accounts(id) on delete cascade,
+
+    employee_code varchar(20) not null unique,
+    department varchar(100),
+    internal_note text,
+
+    created_at timestamptz not null default now(),
+    updated_at timestamptz,
+    deleted_at timestamptz
+);
