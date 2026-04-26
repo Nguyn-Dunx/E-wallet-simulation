7d2e6cc57c78da47050178569251a83b5bb99ffb
HuyQuang075 <88672686+NoBugNoPain404@users.noreply.github.com>
2026-04-26 09:30:50 +0700

    Refactor and add unique indexes in optimization_indexes.sql
    Updated unique indexes and added new indexes for users, admin, and accounts.

--- database/indexes/optimization_indexes.sql
+++ database/indexes/optimization_indexes.sql
@@ -1,10 +1,25 @@
-create unique index idx_unique_phone
+-- USERS
+create unique index idx_unique_users_phone
 on users(phone)
 where deleted_at is null;
 
-create unique index idx_unique_account_id
-on account_role(account_id)
-include (role_id);
+-- ADMIN
+create unique index idx_unique_admin_employee_code
+on admin(employee_code)
+where deleted_at is null;
 
-create unique index idx_unique_employee_code
-on admin(employee_code);
+-- ACCOUNTS
+create unique index idx_unique_accounts_login_key
+on accounts(login_key)
+where deleted_at is null;
+
+create index idx_accounts_role_id
+on accounts(role_id);
+
+create index idx_accounts_not_deleted
+on accounts(id)
+where deleted_at is null;
+
+-- FK indexes
+create index idx_users_id on users(id);
+create index idx_admin_id on admin(id);
