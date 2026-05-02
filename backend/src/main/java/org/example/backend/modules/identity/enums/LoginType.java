c7fe9449972da7e911995c20101734bf165c5824
DESKTOP-ODU9UF9\ASUS <hehwoh@gmail.com>
2026-05-02 12:43:02 +0700

    fix conflict

--- backend/src/main/java/org/example/backend/modules/identity/enums/LoginType.java
+++ backend/src/main/java/org/example/backend/modules/identity/enums/LoginType.java
@@ -1,8 +1,5 @@
-<<<<<<<< HEAD:backend/src/main/java/org/example/backend/modules/identity/enums/LoginType.java
+
 package org.example.backend.modules.identity.enums;
-========
-package org.example.backend.modules.identity.common;
->>>>>>>> e5cbe51a1013d5f92df64cf259fa8317213943b8:backend/src/main/java/org/example/backend/modules/identity/common/LoginType.java
 
 public enum LoginType {
     PHONE,
--- backend/src/main/java/org/example/backend/security/enums/RoleName.java
+++ backend/src/main/java/org/example/backend/security/enums/RoleName.java
@@ -1,8 +1,5 @@
-<<<<<<<< HEAD:backend/src/main/java/org/example/backend/security/enums/RoleName.java
+
 package org.example.backend.security.enums;
-========
-package org.example.backend.modules.identity.common;
->>>>>>>> e5cbe51a1013d5f92df64cf259fa8317213943b8:backend/src/main/java/org/example/backend/modules/identity/common/RoleName.java
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
