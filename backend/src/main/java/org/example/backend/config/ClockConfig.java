fa9e27fd31dbe216e09b15f5ca35252f4c2391ae
DESKTOP-ODU9UF9\ASUS <hehwoh@gmail.com>
2026-05-09 10:07:56 +0700

    ignore log, fix application.yaml , finish api getMyWallet

--- backend/src/main/java/org/example/backend/config/ClockConfig.java
+++ backend/src/main/java/org/example/backend/config/ClockConfig.java
@@ -1,15 +1,31 @@
 package org.example.backend.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Configuration
+@Slf4j
 public class ClockConfig {
 
     @Bean
     public Clock clock() {
         return Clock.systemUTC();
+    }
+
+    @Bean
+    public CommandLineRunner verifyClockOnStartup(Clock clock) {
+        return args -> {
+            log.info("=====================================================");
+            log.info("VN Time: {}",
+                    ZonedDateTime.now(ZoneId.of("Asia/Bangkok")));
+            log.info("=====================================================");
+        };
     }
 }
--- backend/src/main/java/org/example/backend/modules/wallet/repository/WalletRepository.java
+++ backend/src/main/java/org/example/backend/modules/wallet/repository/WalletRepository.java
@@ -1,17 +1,16 @@
 package org.example.backend.modules.wallet.repository;
 
 import org.example.backend.modules.wallet.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface WalletRepository {
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
     Optional<Wallet> findByUserIdAndDeletedAtIsNull(UUID userId);
 
     Optional<Wallet> findByIdAndDeletedAtIsNull(UUID id);
-
-    Wallet save(Wallet newWallet);
 }
